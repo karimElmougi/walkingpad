f327a9a00026ae5ae76e5cda168f0b26fcdb5c4d
karim <karim.elmougi@gmail.com>
2022-03-12 18:23:19 -0500

    Update the prototype

--- walkingpad_protocol/examples/prototype.rs
+++ walkingpad_protocol/examples/prototype.rs
@@ -1,15 +1,22 @@
-use walkingpad_protocol::request::Command;
-use walkingpad_protocol::response::Response;
-use walkingpad_protocol::*;
+use walkingpad_protocol::response::StoredStats;
+use walkingpad_protocol::{Request, Response};
 
 use std::error::Error;
 use std::time::Duration;
 
+use btleplug::api::bleuuid::uuid_from_u16;
 use btleplug::api::Peripheral as _;
 use btleplug::api::{Central, ScanFilter, WriteType};
 use btleplug::api::{Characteristic, Manager as _};
 use btleplug::platform::{Adapter, Manager, Peripheral};
 use futures::stream::StreamExt;
+use uuid::Uuid;
+
+pub const WALKINGPAD_SERVICE_UUID: Uuid = uuid_from_u16(0xfe00);
+
+pub const TREADMILL_READ_CHARACTERISTIC_UUID: Uuid = uuid_from_u16(0xfe01);
+
+pub const TREADMILL_CHARACTERISTIC_UUID: Uuid = uuid_from_u16(0xfe02);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -37,17 +44,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .find(|c| c.uuid == TREADMILL_READ_CHARACTERISTIC_UUID)
         .unwrap();
 
-    println!("Waking up");
-    send(
-        Command::SetMode(Mode::Manual),
-        &walkingpad,
-        write_characteristic,
-    )
-    .await;
-    tokio::time::sleep(Duration::from_secs(1)).await;
+    // println!("Waking up");
+    // send(
+    //     Request::SetMode(Mode::Manual),
+    //     &walkingpad,
+    //     write_characteristic,
+    // )
+    // .await;
+    // tokio::time::sleep(Duration::from_secs(1)).await;
 
     println!("Subscribing");
     walkingpad.subscribe(read_characteristic).await.unwrap();
+
+    println!("Fetching past stats");
+    let stats = get_stats(&walkingpad, write_characteristic).await;
+    for stats in stats {
+        println!("{:?}", stats);
+    }
 
     tokio::spawn({
         let walkingpad = walkingpad.clone();
@@ -62,43 +75,72 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    // println!("Starting");
-    // send(Command::Start, &walkingpad, write_characteristic).await;
-    // tokio::time::sleep(Duration::from_secs(10)).await;
-
-    for _ in 0..3 {
-        println!("Sending query");
-        send(Command::QueryStoredRuns, &walkingpad, write_characteristic).await;
-        tokio::time::sleep(Duration::from_secs(5)).await;
-    }
-
-    // let response = walkingpad.read(read_characteristic).await.unwrap();
-    // println!("{:?}", Response::parse(response.as_slice()).unwrap());
-
-    // println!("Stopping");
-    // send(Command::Stop, &walkingpad, write_characteristic).await;
-    // tokio::time::sleep(Duration::from_secs(5)).await;
-
-    println!("Setting to sleep");
+    tokio::time::sleep(Duration::from_secs(5)).await;
     send(
-        Command::SetMode(Mode::Sleep),
+        &Request::get().settings(),
         &walkingpad,
         write_characteristic,
     )
     .await;
 
+    tokio::time::sleep(Duration::from_secs(5)).await;
+    send(&Request::get().state(), &walkingpad, write_characteristic).await;
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
     Ok(())
 }
 
-async fn send(command: Command, walkingpad: &Peripheral, write_characteristic: &Characteristic) {
+async fn send(command: &[u8], walkingpad: &Peripheral, write_characteristic: &Characteristic) {
     walkingpad
-        .write(
-            write_characteristic,
-            &command.as_bytes(),
-            WriteType::WithoutResponse,
-        )
+        .write(write_characteristic, command, WriteType::WithoutResponse)
         .await
         .unwrap()
+}
+
+async fn get_stats(
+    walkingpad: &Peripheral,
+    write_characteristic: &Characteristic,
+) -> Vec<StoredStats> {
+    let mut stream = walkingpad.notifications().await.unwrap();
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    send(
+        &Request::get().latest_stored_stats(),
+        &walkingpad,
+        write_characteristic,
+    )
+    .await;
+
+    let mut stats = vec![];
+    while let Some(data) = stream.next().await {
+        match Response::parse(data.value.as_slice()) {
+            Ok(Response::StoredStats(stored_stats)) => {
+                stats.push(stored_stats);
+                if let Some(next_id) = stored_stats.next_id {
+                    send(
+                        &Request::get().stored_stats(next_id),
+                        &walkingpad,
+                        &write_characteristic,
+                    )
+                    .await;
+                } else {
+                    send(&Request::clear_stats(), &walkingpad, &write_characteristic).await;
+
+                    return stats;
+                }
+            }
+            Err(err) => {
+                println!("err: {}, data: {:?}", err, data.value);
+                continue;
+            }
+            _ => {
+                continue;
+            }
+        }
+    }
+
+    stats
 }
 
 async fn discover_walkingpad(central: &Adapter) -> Peripheral {
